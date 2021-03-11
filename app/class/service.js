98ea926664852af7654085ad574af42b1d70938b
spotentier <sam.potentier@sts-sio-caen.info>
2021-03-11 11:58:04 +0100

    td2 début

---
+++ app/class/service.js
@@ -0,0 +1,11 @@
+export default class Service {
+  services = [];
+
+  constructor(services) {
+    this.services = services;
+  }
+
+  get countActive() {}
+
+  get sumActive() {}
+}
--- app/router.js
+++ app/router.js
@@ -1,9 +1,17 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'tds/config/environment';
 
+export default class Ex2Route extends Route {
+  model() {
+    return new Services(datas);
+  }
+}
+
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('service');
+});
